c06904873453e26e8a30a31865160cf86b057712
commitwithrudra <progrudra@gmail.com>
2026-04-29 14:00:52 +0530

    Update app.js7

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.get('/', (req, res) => {
 
 const ERPNEXT_API_URL = "https://unheard-ducky-profile.ngrok-free.dev/api/method/rudra_utility.api.update_whatsapp_status";
 // Route for POST requests
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
