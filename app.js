9d2f955d7cf750f24a8ca325072a1f6f44537d01
commitwithrudra <progrudra@gmail.com>
2026-06-08 10:15:45 +0530

    latest update 08-06 10:15

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.get('/', (req, res) => {
   }
 });
 
-const ERPNEXT_API_URL = "https://unheard-ducky-profile.ngrok-free.dev/api/method/rudra_utility.api.update_whatsapp_status";
+const ERPNEXT_API_URL = "https://lvsm.co.in//api/method/rudra_utility.api.update_whatsapp_status";
 // const ERPNEXT_API_URL = "https://prochouse.frappe.cloud/api/method/rudra_utility.api.update_whatsapp_status";
 // Route for POST requests
 app.post('/', async (req, res) => {
